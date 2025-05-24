397c4e8c8e409c8d876d7d347e798f48bc3279a0
EdMaster21 <eduardo.gomezm@outlook.com>
2025-05-24 10:37:12 -0600

    Add files via upload

---
+++ roles.sql
@@ -0,0 +1,6 @@
+BEGIN
+  INSERT INTO ROL (NOMBRE_ROL) VALUES ('Encargado de almacén');
+  INSERT INTO ROL (NOMBRE_ROL) VALUES ('Jefe de instalaciones');
+  INSERT INTO ROL (NOMBRE_ROL) VALUES ('Jefe de mantenimiento');
+  INSERT INTO ROL (NOMBRE_ROL) VALUES ('Jefe de finanzas');
+END;
