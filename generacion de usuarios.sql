c86fd15ccf0764a75a95e0b171543a63fc2043cf
EdMaster21 <eduardo.gomezm@outlook.com>
2025-05-24 10:43:18 -0600

    Update generacion de usuarios.sql
    Cambios a la sintaxis

--- generacion de usuarios.sql
+++ generacion de usuarios.sql
@@ -1,10 +1,11 @@
 BEGIN
-    APEX_UTIL.CREATE_USER(
-        p_user_name     => 'fernando',
-        p_web_password  => '123',
-        p_email_address => 'fernando.gomez@netowak.com',
-        p_developer_privs => 'END_USER',
-        p_default_schema => 'gmtechnology', 
-        p_change_password_on_first_use => 'N'
-    );
+  APEX_UTIL.CREATE_USER(
+    p_user_name     => 'Javier',
+    p_web_password  => '123',
+    p_email_address => 'javier@ejemplo.com',
+    p_developer_privs => 'END_USER',
+    p_default_schema => USER,
+    p_change_password_on_first_use => 'N'
+  );
 END;
+
